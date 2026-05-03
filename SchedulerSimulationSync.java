da232b35da49e43dde73a59d4e38ddfe52b0b70a
Rahaf1230 <445052029@std.psau.edu.sa>
2026-05-03 21:42:48 +0300

    Add ReentrantLouck and import package

--- SchedulerSimulationSync.java
+++ SchedulerSimulationSync.java
@@ -5,7 +5,10 @@ import java.util.HashMap;
 import java.util.Random;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.locks.ReentrantLock;
 
+    
+}
 // ANSI Color Codes for enhanced terminal output
 class Colors {
     public static final String RESET = "\u001B[0m";
@@ -38,6 +41,8 @@ class SharedResources {
 
     // TODO #1: Add a ReentrantLock(s) here to protect critical sections
     // Example: public static final ReentrantLock lock = new ReentrantLock();
+     //Added ReentrantLouk for protecting shared resources
+     public static final ReentrantLock lock=new ReentrantLock();
 
     // TODO #2: Add a Semaphore to limit concurrent process execution
     // Example: public static final Semaphore cpuSemaphore = new Semaphore(1);
